283159698ff0ee8d3430d2596c48b802b5bd5e0a
gncdev <gncdev@zerez.com>
2025-03-11 12:26:39 -0500

    ✨ add site config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  siteTitle: 'Nombre para el blog',
+  siteTagline: 'palabra, clave,...',
+  archiveTitle: 'Colección',
+  dateFormat: 'MMM dd, yyyy',
+};
+
+export default config;
